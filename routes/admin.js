72aca88aba9a2fc5950081ea7ee6c4e394245cef
catter <terter1949@hotmail.com>
2018-02-21 18:09:39 +0700

    make it better

--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,15 @@ var request = require('request-promise');
 var moment = require('moment');
 var router = express.Router();
 var user = require("../controllers/UserController.js");
+var morcyc = require("../controllers/MotorController.js");
+
 router.get('/', function (req, res, next) {
     res.render("admin/index");
 });
 router.get("/listname",function(req,res,next){
     user.userList(req,res);
+});
+router.get("/listmotorcycle",function(req,res,next){
+    morcyc.morcycList(req,res);
 });
 module.exports = router;
