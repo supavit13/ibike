92571e401e0a42d4d53c1169c4dff5da50890979
supavit13 <supavit.n@ku.th>
2018-03-19 04:24:54 +0700

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,42 @@ var User = require("../models/User");
 var morcyc = require("../controllers/MotorController.js");
 var admin = require("../controllers/AdminController.js");
 
+function isAdmin(req,res){
+    if(req.session.userId){
+        User.findById({ _id : req.session.userId}).exec(function(err,user){
+            if(user["pass"] == "Admin"){
+                return true;
+            }else{
+                return false;
+            }
+        });
+    }else {
+        return false;
+    }
+}
 router.get('/', function (req, res, next) {
-    admin.plotToMapAdmin(req,res);
+    if(isAdmin(req,res)){
+        admin.plotToMapAdmin(req,res);
+    }else {
+        res.redirect("/");
+    }
+    
 });
 router.get("/listname",function(req,res,next){
-    admin.userList(req,res);
+    if(isAdmin(req,res)){
+        admin.userList(req,res);
+    }else {
+        res.redirect("/");
+    }
+    
 });
 router.get("/listmotorcycle",function(req,res,next){
-    admin.morcycList(req,res);
+    if(isAdmin(req,res)){
+        admin.morcycList(req,res);
+    }else {
+        res.redirect("/");
+    }
+    
 });
 router.post("/add",function(req,res){
     console.log(req.body.id);
