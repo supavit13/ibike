02c4e8b9e0f817f303cb454a9c6b0ea1117f3c2b
supavit13 <supavit.n@ku.th>
2018-04-19 23:09:05 +0700

    commit

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -262,6 +262,7 @@ AdminController.checkzone = function(req,res){
     console.log(motorcycID);
     console.log(latlng);
     console.log(req.body);
+    console.log(res.body);
    
 }
 module.exports = AdminController;
