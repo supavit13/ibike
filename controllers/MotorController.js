64e7213b4130f5f81a8e578f71b88be367716b50
supavit13 <supavit.n@ku.th>
2018-04-25 16:56:47 +0700

    commit

--- controllers/MotorController.js
+++ controllers/MotorController.js
@@ -143,6 +143,12 @@ MotorController.turnOff = function (req, res) {
     console.log(latlng);
     if (req.session.morcycId != null) {
         var userId = req.session.userId;
+        if(req.body.msg=="cancelBooking"){
+            Motorcycle.findById({ _id: req.session.morcycId }).update({ using : "no"},function(err,result){
+                if(err) res.send(err);
+                res.send(true);
+            });
+        }
         Motorcycle.findById({ _id: req.session.morcycId }).update({ using: "no" , latlng : latlng }, function (err, result) {
             // console.log("sss");
             if (err) res.send(err);
